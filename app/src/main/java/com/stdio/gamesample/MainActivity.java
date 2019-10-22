59667fb3f5cc7dfad763fce8a44c7564e58ec5f9
Stdio <kwork-stdio@mail.ru>
2019-10-22 12:18:01 +0300

    0.0.1

--- app/src/main/java/com/stdio/gamesample/MainActivity.java
+++ app/src/main/java/com/stdio/gamesample/MainActivity.java
@@ -2,7 +2,9 @@ package com.stdio.gamesample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
